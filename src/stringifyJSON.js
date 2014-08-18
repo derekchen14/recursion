41684168ad7f7a35e1057726ba2a1a896376b245
Derek Chen <derekchen14@gmail.com>
2014-08-18 13:22:32 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,52 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  switch(typeof obj) {
+    case "string":
+      return '"'+obj+'"'; break;
+    case "boolean":
+      return obj ? 'true' : 'false'
+    case "number":
+      return ''+obj; break;
+    case "function":
+      throw new Error('Unstringifiable!'); break;
+    case "object":
+      try {
+        if (obj === null) {
+          return 'null';
+        } else if (obj.length > -1) {
+          return resolveArrays(obj);
+        } else {
+          return resolveDictionaries(obj);
+        }
+      } catch(err) {
+        console.log(err.message);
+        return '{}';
+      }
+      break;
+  };
+};
+
+var resolveArrays = function(obj) {
+  if (obj.length === 0) {
+    return '[]';
+  } else {
+    var result = obj.reduce(function(memo, thing){
+
+      return memo+stringifyJSON(thing)+',';
+    }, '[');
+    return result.slice(0,-1)+']';
+  }
+};
+
+var resolveDictionaries = function(obj) {
+  var k = Object.keys(obj);
+  if (k.length === 0) {
+    return '{}';
+  } else {
+    var result = k.reduce(function(j, thing){
+      return j+stringifyJSON(thing)+':'+stringifyJSON(obj[thing])+',';
+    }, '{');
+    return result.slice(0,-1)+'}';
+  }
 };
